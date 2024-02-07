dab5336359fa3e8f1328e8af05d99c236260e8bb
Ronney Arias <ronneyarias@gmail.com>
2024-02-07 12:34:38 -0600

    lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isItem, setItem] = useState(false);
+
+  function handleClick() {
+    setItem((isItem) => !isItem)
+  }
+
+  const itemClass = isItem ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={`add ${isItem ? "remove" : ""}`}>{isItem ? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState("All")
+
+  const itemDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  }) 
+  function handleFilterChange(event){
+    setCategory(event.target.value);
+  }
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +34,17 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+// ## Filter
+
+// In the `ShoppingList` component, there is a `<select>` element that will allow
+// us to **filter** the list of items by category.
+
+// Use the `useState` hook to create a state variable called `selectedCategory` for
+// keeping track of the selected value from this `<select>` element. When the value
+// of the `<select>` element is **changed**, update state.
+
+// You should also use the `selectedCategory` variable to determine which items to
+// display in the shopping list. You'll need some way of **filter**ing the array of
+// items based on the `selectedCategory`.
